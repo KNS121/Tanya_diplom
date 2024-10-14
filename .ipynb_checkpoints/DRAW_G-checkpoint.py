e3aa9c409fca354ca863f40c809370e3045dd939
KNS_121 <komaroff.ns@gmail.com>
2024-10-14 18:44:50 +0300

    First_pictrures1

--- .ipynb_checkpoints/DRAW_G-checkpoint.py
+++ .ipynb_checkpoints/DRAW_G-checkpoint.py
@@ -1,9 +1,25 @@
+
+
 import matplotlib.pyplot as plt
 
-def draw(x,y, color_, title, x_label, y_label):
-    plt.plot(x,y,color=color_)
-    plt.plot(x,y,color=color_)
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.show()
+class Plotter:
+    def __init__(self, x=None, y=None, color='blue', title='', x_label='', y_label='', grid=True):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
+        self.grid = grid
+
+    def draw(self):
+        if self.x is None or self.y is None:
+            raise ValueError("Data (x and y) must be set before drawing the plot.")
+
+        plt.plot(self.x, self.y, color=self.color)
+        plt.title(self.title)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        if self.grid:
+            plt.grid(True)
+        plt.show()
